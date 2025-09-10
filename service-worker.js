03ba37c3306c02f70a0f3ca27321c2468aa0dba5
JewMen <82110988+JewMen@users.noreply.github.com>
2025-09-10 21:27:22 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'note-app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+  // 這裡可以加入其他你想快取的檔案，例如 icons/
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 從快取中回傳資源
+        if (response) {
+          return response;
+        }
+        // 如果快取中沒有，則發送網路請求
+        return fetch(event.request);
+      })
+  );
+});
